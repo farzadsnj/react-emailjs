996c7271dc89925dd9dd5bf2071dcb3fc7b045f3
Farzad Sanjarani <farzadsanjarani3@gmail.com>
2023-06-22 11:41:06 +1000

    co1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <form id="contact" action="" method="post">
+          <h3>Colorlib Contact Form</h3>
+          <h4>Contact us for custom quote</h4>
+
+          <input
+            placeholder="Your name"
+            type="text"
+            tabindex="1"
+            required
+            autofocus
+          />
+          <input
+            placeholder="Your Email Address"
+            type="email"
+            tabindex="2"
+            required
+          />
+
+          <input
+            placeholder="Your Phone Number (optional)"
+            type="tel"
+            tabindex="3"
+            required
+          />
+
+          <input
+            placeholder="Your Web Site (optional)"
+            type="url"
+            tabindex="4"
+            required
+          />
+
+          <textarea
+            placeholder="Type your message here...."
+            tabindex="5"
+            required
+          ></textarea>
+
+          <button
+            name="submit"
+            type="submit"
+            id="contact-submit"
+            data-submit="...Sending"
+          >
+            Submit
+          </button>
+          <p class="copyright">
+            Designed by{"Farzad Sanjarani"}
+            {/* <a href="https://colorlib.com" target="_blank" title="Colorlib"> */}
+              Colorlib
+            {/* </a> */}
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
